0d722ce63c0a90f30198b9da781f331cdd73a26f
yeshoham <yeshoham@cisco.com>
2018-10-27 17:00:29 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -106,6 +106,16 @@ UserSchema.pre('save', function (next) {
     }
 });
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
